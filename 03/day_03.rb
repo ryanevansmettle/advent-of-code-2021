35ab7d662b35e1921ba8a8457fb6e18b3e2e62ad
Ryan Evans <ryan.evans@mettle.co.uk>
2021-12-03 09:05:23 +0000

    Tidy up a bit.

--- 03/day_03.rb
+++ 03/day_03.rb
@@ -4,65 +4,90 @@ require_relative '../lib/util'
 
 input = Input.read_lines
 
+# @param [Array<String>] binary_strings
+# @param [Numeric] position
+# @return [Hash]
 def frequency_bucket_at_position(binary_strings, position)
   freq = { "0" => 0, "1" => 0 }
-  binary_strings.each { |binstr|
-    bit = binstr[position]
-    freq[bit] = freq[bit] + 1
+  binary_strings.each { |bitstring|
+    freq[bitstring[position]] += 1
   }
   freq
 end
 
+# @param [Hash] frequency_bucket
+# @return [String]
 def most_common(frequency_bucket)
-  frequency_bucket.max_by { |k| k[1] }[0]
+  frequency_bucket.max_by { |f| f[1] }[0]
 end
 
+# @param [Hash] frequency_bucket
+# @return [String]
 def least_common(frequency_bucket)
-  frequency_bucket.min_by { |k| k[1] }[0]
+  frequency_bucket.min_by { |f| f[1] }[0]
 end
 
+# @param [Hash] frequency_bucket
+# @return [Boolean]
 def equally_common(frequency_bucket)
-  frequency_bucket["0"] == frequency_bucket["1"]
+  frequency_bucket.values.uniq.count == 1
 end
 
+# @param [Array<String>] binary_strings
+# @param [Numeric] position
+# @return [String]
 def ogr_criteria(binary_strings, position)
   freq_bucket = frequency_bucket_at_position(binary_strings, position)
   return "1" if equally_common(freq_bucket)
   most_common(freq_bucket)
 end
 
+# @param [Array<String>] binary_strings
+# @param [Numeric] position
+# @return [String]
 def c02_criteria(binary_strings, position)
   freq_bucket = frequency_bucket_at_position(binary_strings, position)
   return "0" if equally_common(freq_bucket)
   least_common(freq_bucket)
 end
 
+# @param [Array<String>] binary_strings
+# @return [String]
 def ogr(binary_strings)
-  num_bits = binary_strings[0].length
-
-  remaining_strings = binary_strings.clone
-
-  (0..num_bits - 1).each { |pos|
-    ogr_criteria = ogr_criteria(remaining_strings, pos)
-    remaining_strings = remaining_strings.filter { |s| s[pos] == ogr_criteria }
-
-    break if remaining_strings.size == 1
-  }
-  remaining_strings[0]
+  _ogr(binary_strings.clone, 0)
 end
 
+# @param [Array<String>] remaining_strings
+# @param [Numeric>] position
+# @return [String]
+def _ogr(remaining_strings, position)
+  ogr_criteria = ogr_criteria(remaining_strings, position)
+  remaining_strings = remaining_strings.filter { |s| s[position] == ogr_criteria }
+
+  if remaining_strings.size == 1
+    return remaining_strings[0]
+  end
+  _ogr(remaining_strings, position += 1)
+end
+
+# @param [Array<String>] binary_strings
+# @return [String]
 def c02(binary_strings)
-  num_bits = binary_strings[0].length
+  _c02(binary_strings.clone, 0)
+end
 
-  remaining_strings = binary_strings.clone
+# @param [Array<String>] remaining_strings
+# @param [Numeric>] position
+# @return [String]
+def _c02(remaining_strings, position)
+  c02_criteria = c02_criteria(remaining_strings, position)
+  remaining_strings = remaining_strings.filter { |s| s[position] == c02_criteria }
 
-  (0..num_bits - 1).each { |pos|
-    c02_criteria = c02_criteria(remaining_strings, pos)
-    remaining_strings = remaining_strings.filter { |s| s[pos] == c02_criteria }
+  if remaining_strings.size == 1
+    return remaining_strings[0]
+  end
 
-    break if remaining_strings.size == 1
-  }
-  remaining_strings[0]
+  _c02(remaining_strings, position += 1)
 end
 
 def part1 (input)
