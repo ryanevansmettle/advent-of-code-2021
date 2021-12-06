360d5a94613889b24372ea07a09bbb0addcc318b
Ryan Evans <ryan.evans@mettle.co.uk>
2021-12-06 11:53:43 +0000

    All solutions extend Scenario.

--- 01/day_01.rb
+++ 01/day_01.rb
@@ -1,23 +1,39 @@
 #!/usr/bin/env ruby
 
-input = File.read("input.txt").split.map { |l| l.to_i }
+require_relative '../lib/util'
 
-# @param [Array] input
-# @return [Array]
-def sequential_increases (input)
-  input.each_cons(2).map { |a, b| [a, b] }.filter { |a, b| b > a }
+class Day01 < Scenario
+
+  # @param [File] input
+  def grok_input(input)
+    Input.to_lines(input).map { |i| i.to_i }
+  end
+
+  # @param [Array] input
+  # @return [Array]
+  def sequential_increases (input)
+    input.each_cons(2).map { |a, b| [a, b] }.filter { |a, b| b > a }
+  end
+
+  # @param [Array] input
+  def part1 (input)
+    sequential_increases(input).size
+  end
+
+  def part1_expected_result
+    7
+  end
+
+  # @param [Array] input
+  def part2 (input)
+    window_sums = input.each_cons(3).map { |w| w.sum }
+    sequential_increases(window_sums).size
+  end
+
+  def part2_expected_result
+    5
+  end
+
 end
 
-# @param [Array] input
-def part1 (input)
-  sequential_increases(input).size
-end
-
-# @param [Array] input
-def part2 (input)
-  window_sums = input.each_cons(3).map { |w| w.sum }
-  sequential_increases(window_sums).size
-end
-
-puts "Part 1: #{part1(input)}"
-puts "Part 2: #{part2(input)}"
+Day01.new
--- 02/day_02.rb
+++ 02/day_02.rb
@@ -2,76 +2,96 @@
 
 require_relative '../lib/util'
 
-class Instruction
-  # @return [Symbol]
-  attr_accessor :direction
-  # @return [Numeric]
-  attr_accessor :units
+class Day02 < Scenario
 
-  def initialize(direction, units)
-    @direction = direction
-    @units = units
-  end
-end
-
-# @param [String] instruction
-# @return [Instruction]
-def parse_instruction (instruction)
-  (d, n) = instruction
-  case d
-  when "forward"
-    dir = :fwd
-  when "down"
-    dir = :down
-  else
-    dir = :up
+  # @param [File] input
+  def grok_input(input)
+    Input.read_lines_and_tokenise(input)
+         .map(&method(:parse_instruction))
   end
 
-  Instruction.new(dir, n.to_i)
-end
+  class Instruction
+    # @return [Symbol]
+    attr_accessor :direction
+    # @return [Numeric]
+    attr_accessor :units
 
-input = Input.read_lines_and_tokenise
-             .map(&method(:parse_instruction))
-
-# @param [Array<Instruction>] input
-def part1 (input)
-  depth = 0
-  horizontal_position = 0
-
-  input.each { |instruction|
-    case instruction.direction
-    when :down
-      depth += instruction.units
-    when :up
-      depth -= instruction.units
-    else
-      horizontal_position += instruction.units
+    def initialize(direction, units)
+      @direction = direction
+      @units = units
     end
-  }
+  end
 
-  depth * horizontal_position
-end
-
-# @param [Array<Instruction>] input
-def part2 (input)
-  depth = 0
-  horizontal_position = 0
-  aim = 0
-
-  input.each { |instruction|
-    case instruction.direction
-    when :down
-      aim += instruction.units
-    when :up
-      aim -= instruction.units
+  # @param [String] instruction
+  # @return [Instruction]
+  def parse_instruction (instruction)
+    (d, n) = instruction
+    case d
+    when "forward"
+      dir = :fwd
+    when "down"
+      dir = :down
     else
-      horizontal_position += instruction.units
-      depth += aim * instruction.units
+      dir = :up
     end
-  }
 
-  depth * horizontal_position
+    Instruction.new(dir, n.to_i)
+  end
+
+  # @param [Array] input
+  # @return [Array]
+  def sequential_increases (input)
+    input.each_cons(2).map { |a, b| [a, b] }.filter { |a, b| b > a }
+  end
+
+  # @param [Array<Instruction>] input
+  def part1 (input)
+    depth = 0
+    horizontal_position = 0
+
+    input.each { |instruction|
+      case instruction.direction
+      when :down
+        depth += instruction.units
+      when :up
+        depth -= instruction.units
+      else
+        horizontal_position += instruction.units
+      end
+    }
+
+    depth * horizontal_position
+  end
+
+  def part1_expected_result
+    150
+  end
+
+  # @param [Array<Instruction>] input
+  def part2 (input)
+    depth = 0
+    horizontal_position = 0
+    aim = 0
+
+    input.each { |instruction|
+      case instruction.direction
+      when :down
+        aim += instruction.units
+      when :up
+        aim -= instruction.units
+      else
+        horizontal_position += instruction.units
+        depth += aim * instruction.units
+      end
+    }
+
+    depth * horizontal_position
+  end
+
+  def part2_expected_result
+    900
+  end
+
 end
 
-puts "Part 1: #{part1(input)}"
-puts "Part 2: #{part2(input)}"
+Day02.new
